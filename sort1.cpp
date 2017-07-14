65d945f02448bb4147a0e7e58f4df86a516b6776
Eric Matt <erichmatt@gmail.com>
2017-07-14 15:04:52 -0400

    First commit, it just prints the unsorted list

---
+++ sort1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int unsortedlist[20] = {16, 9, 6, 19, 13, 14, 8, 20, 3, 12, 11, 4, 15, 18, 5, 7, 10, 17, 1, 2};
+
+
+
+void printlist()
+{
+for (int i = 0; i < 20; i++)
+{
+cout <<unsortedlist[i] << ',';
+}
+}
+
+int main()
+{
+printlist();
+}
