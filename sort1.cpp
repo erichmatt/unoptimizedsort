b84d018d893d3038d86d3ab23b490d02a5ee981c
Cyrus B <cyrusb252@gmail.com>
2017-07-14 19:50:52 -0400

    Update sort1.cpp
    Possibly put swap function to use, needs work still. Also hi eric, since only you will read this.

--- sort1.cpp
+++ sort1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int unsortedlist[20] = {16, 9, 6, 19, 13, 14, 8, 20, 3, 12, 11, 4, 15, 18, 5, 7, 10, 17, 1, 2};
-
+int sortedlist[20] = //ordered list, variables possibly?
 
 
 void printlist()
@@ -18,6 +18,6 @@ cout << '\n';
 int main()
 {
 printlist();
-swap (unsortedlist [0], unsortedlist[1]);
+swap (unsortedlist [0], sortedlist[1]); //Maaaaybeee??
 printlist();
 }
