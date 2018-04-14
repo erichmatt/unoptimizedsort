5299fc0c7db2cdcffd57f0cdb6d32711990cb25a
Eric Matt <erichmatt@gmail.com>
2018-04-14 19:42:13 -0400

    I have no idea all the things I changed but it works now

--- bubble2.c
+++ bubble2.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 
 int tbs[] = {4,3,2,5};
-int x = 0;
-int swap(){
+int sorta(){
 int c;
 c = 0; 
-
-for(c;c<4;c++){
-	x = 0;
-	if(tbs[c] >= tbs[c+1]){
+int x=1;
+for(c;c<3;c++){
+	printf("Doing a pass");
+	if(tbs[c] > tbs[c+1]){
 	tbs[c+1] = tbs[c+1]^tbs[c];
 	tbs[c] = tbs[c]^tbs[c+1];
 	tbs[c+1] = tbs[c+1]^tbs[c];
+	//swap(tbs[c], tbs[c+1]);
 	x = 0;}
-	else{x=1;}
+	
 }
+return x;
 }
 int main(){
 int i;
 i=0;
+int x = 0;
 while(x == 0){
-swap();
+x = sorta();
 }
-for(i;i<7;i++){
+for(i;i<4;i++){
 printf("%d,",tbs[i]);
 }
 }
