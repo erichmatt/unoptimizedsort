a96366f97281bc16bc9982812ddc06824e3cf659
Eric Matt <erichmatt@gmail.com>
2018-04-14 20:59:12 -0400

    removed return

--- bubble2.c
+++ bubble2.c
@@ -1,28 +1,25 @@
 #include<stdio.h>
 
 int tbs[] = {4,3,2,5};
+int x = 0;
 int sorta(){
 int c;
 c = 0; 
-int x=1;
+x = 1;
 for(c;c<3;c++){
-	printf("Doing a pass");
 	if(tbs[c] > tbs[c+1]){
 	tbs[c+1] = tbs[c+1]^tbs[c];
 	tbs[c] = tbs[c]^tbs[c+1];
 	tbs[c+1] = tbs[c+1]^tbs[c];
 	//swap(tbs[c], tbs[c+1]);
 	x = 0;}
-	
 }
-return x;
 }
 int main(){
 int i;
 i=0;
-int x = 0;
 while(x == 0){
-x = sorta();
+sorta();
 }
 for(i;i<4;i++){
 printf("%d,",tbs[i]);
